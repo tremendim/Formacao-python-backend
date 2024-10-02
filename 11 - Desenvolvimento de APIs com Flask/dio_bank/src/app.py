d2bc9ed002ce8004a49fa419bb9e328b27cba9d1
Tremendim <luisfernandobeloti@gmail.com>
2024-10-02 16:52:37 -0300

    Configurando conexão do BD com SQLAlchemy

--- 11 - Desenvolvimento de APIs com Flask/dio_bank/src/app.py
+++ 11 - Desenvolvimento de APIs com Flask/dio_bank/src/app.py
@@ -1,14 +1,28 @@
 import os
 
-from flask import Flask
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+import click
 
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+@click.command('init-db')
+def init_db_command():
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo('Initialized the database.')
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join('diobank.sqlite'),
+        SQLALCHEMY_DATABASE_URI="sqlite:///diobank.sqlite"
     )
 
     if test_config is None:
@@ -18,8 +32,15 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
+    try: 
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    #Resgitrando cli commands
+    app.cli.add_command(init_db_command)
 
-    from . import db
+    #Inicializando extensões
     db.init_app(app)
 
     return app
