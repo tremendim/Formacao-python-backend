249a273dc20635de7e30227d781a93c1d8a79ed9
Tremendim <luisfernandobeloti@gmail.com>
2024-11-07 14:36:41 -0300

    Operações CRUD - READ

--- 11 - Desenvolvimento de APIs com Flask/dio_bank/src/controllers/user.py
+++ 11 - Desenvolvimento de APIs com Flask/dio_bank/src/controllers/user.py
@@ -11,11 +11,20 @@ def _create_user():
     db.session.add(user)
     db.session.commit()
 
+
+def _list_users():
+    query = db.select(User)
+    results = db.session.execute(query).scalars()
+    return [
+        {'id':result.id, 'username': result.username}
+        for result in results 
+    ]
+
 @app.route('/', methods=['GET','POST'])
 def handle_user():
     if request.method == 'POST':
         _create_user()
         return {'message': 'User created!'}, HTTPStatus.CREATED
     else:
-        return {"users": []}
+        return {"users": _list_users()}
     return
