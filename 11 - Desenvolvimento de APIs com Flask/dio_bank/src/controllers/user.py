2607f661f1bfad8a8bca25206b1fd49c9209a60a
Tremendim <luisfernandobeloti@gmail.com>
2024-11-07 15:08:00 -0300

    Operações CRUD - DELETE

--- 11 - Desenvolvimento de APIs com Flask/dio_bank/src/controllers/user.py
+++ 11 - Desenvolvimento de APIs com Flask/dio_bank/src/controllers/user.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from src.app import User, db
 from http import HTTPStatus
+from sqlalchemy import inspect
 
 
 app = Blueprint('user', __name__, url_prefix='/users')
@@ -28,3 +29,32 @@ def handle_user():
     else:
         return {"users": _list_users()}
     return
+
+@app.route('/<int:user_id>')
+def get_user(user_id):
+    user = db.get_or_404(User, user_id)
+    return [
+        {'id':user.id, 'username': user.username}
+    ]
+
+@app.route('/<int:user_id>', methods=['PATCH'])
+def update_user(user_id):
+    user = db.get_or_404(User, user_id)
+    data = request.json
+
+    mapper = inspect(User)
+    for column in mapper.attrs:
+        if column.key in data:
+            setattr(user, column.key, data[column.key])
+    db.session.commit()
+
+    return [
+        {'id':user.id, 'username': user.username}
+    ]
+
+@app.route('/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = db.get_or_404(User, user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return "", HTTPStatus.NO_CONTENT
