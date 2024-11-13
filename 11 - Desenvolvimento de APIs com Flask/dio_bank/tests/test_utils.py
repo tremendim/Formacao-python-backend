b76abb4630587fdd27bba02358813eee11ad25ee
Tremendim <luisfernandobeloti@gmail.com>
2024-11-13 14:35:12 -0300

    Mockando os dados com Pytest-mock

--- 11 - Desenvolvimento de APIs com Flask/dio_bank/tests/test_utils.py
+++ 11 - Desenvolvimento de APIs com Flask/dio_bank/tests/test_utils.py
@@ -1,6 +1,6 @@
 from src.utils import eleva_quadrado, requires_role
 import pytest
-from unittest.mock import  Mock, patch
+from unittest.mock import  patch
 
 
 @pytest.mark.parametrize("test_input,expected", [(2,4), (10,100), (3,9)])
@@ -20,35 +20,30 @@ def test_eleva_quadrado_falha(test_input,exc_class,msg):
         eleva_quadrado(test_input)
     assert str(exc.value) == msg
 
-def test_requires_role_success():
-    mock_user = Mock()
+def test_requires_role_success(mocker):
+    #Given
+    mock_user = mocker.Mock()
     mock_user.role.name = 'admin'
-    mock_get_jwt_identity = patch('src.utils.get_jwt_identity')
-    mock_db_get_or_404 = patch('src.utils.db.get_or_404', return_value=mock_user)
-    mock_db_get_or_404.start()
-    mock_get_jwt_identity.start()
-
+    mocker.patch('src.utils.get_jwt_identity')
+    mocker.patch('src.utils.db.get_or_404', return_value=mock_user)
     decorated_function = requires_role('admin')(lambda: 'success')
+
+    #When
     result = decorated_function()
 
+    #Then
     assert result == 'success'
 
-    mock_get_jwt_identity.stop()
-    mock_db_get_or_404.stop()
-
-def test_requires_role_fail():
-    mock_user = Mock()
+def test_requires_role_fail(mocker):
+    #Given
+    mock_user = mocker.Mock()
     mock_user.role.name = 'normal'
-    mock_get_jwt_identity = patch('src.utils.get_jwt_identity')
-    mock_db_get_or_404 = patch('src.utils.db.get_or_404', return_value=mock_user)
-    mock_db_get_or_404.start()
-    mock_get_jwt_identity.start()
-
+    mocker.patch('src.utils.get_jwt_identity')
+    mocker.patch('src.utils.db.get_or_404', return_value=mock_user)
     decorated_function = requires_role('admin')(lambda: 'success')
 
+    #When
     result = decorated_function()
 
+    #Then
     assert result == ({"message": "User dont have acess"}, 401)
-
-    mock_get_jwt_identity.stop()
-    mock_db_get_or_404.stop()
