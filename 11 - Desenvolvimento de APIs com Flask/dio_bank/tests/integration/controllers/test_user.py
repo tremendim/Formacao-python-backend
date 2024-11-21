ed4bf7085d752bb3a49621ecef809db7470e8dfe
Tremendim <luisfernandobeloti@gmail.com>
2024-11-21 10:11:34 -0300

    Teste de integração - 2

--- 11 - Desenvolvimento de APIs com Flask/dio_bank/tests/integration/controllers/test_user.py
+++ 11 - Desenvolvimento de APIs com Flask/dio_bank/tests/integration/controllers/test_user.py
@@ -1,6 +1,37 @@
 from http import HTTPStatus
-
+from src.app import User, db,Role
 
 def test_get_user_sucess(client):
-    response = client.get('/users/1')
+    #Given
+    role = Role(name='admin')
+    db.session.add(role)
+    db.session.commit()
+
+    user = User(username='john-doe', password='test', role_id=role.id)
+    db.session.add(user)
+    db.session.commit()
+
+    #When
+    response = client.get(f'/users/{user.id}')
+
+    #Then
     assert response.status_code == HTTPStatus.OK
+    #assert response.json == {"id": user.id, "username": user.username}
+
+
+def test_get_user_not_found(client):
+    #Given
+    role = Role(name='admin')
+    db.session.add(role)
+    db.session.commit()
+    user_id = 1
+
+    #When
+    response = client.get(f'/users/{user_id}')
+
+    #Then
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    #assert response.json == {"id": user.id, "username": user.username}
+
+
+
