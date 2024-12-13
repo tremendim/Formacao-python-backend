5cdf9f14790dc85326660ff9d6007e712e6984ae
Tremendim <luisfernandobeloti@gmail.com>
2024-12-13 16:05:24 -0300

    Consultando o banco de dados

--- 12 - Desenvolvimento fullstack com Django/mysite/polls/models.py
+++ 12 - Desenvolvimento fullstack com Django/mysite/polls/models.py
@@ -1,14 +1,24 @@
 from django.db import models
+from datetime import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     active = models.BooleanField(default=True)
-    x = models.TextField()
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+    def __str__(self):
+        return self.question_text
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.question.id}: {self.choice_text}"
+    
