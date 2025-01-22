86b371af23c79e65ea96ecf27b42b77d88270e4e
Tremendim <luisfernandobeloti@gmail.com>
2025-01-22 10:34:36 -0300

    Path parameter

--- 13 - APIs Assincronas com FastAPI/dio-blog/main.py
+++ 13 - APIs Assincronas com FastAPI/dio-blog/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI 
+from datetime import datetime, UTC
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return{'message': 'Hello World'}
+@app.get('/posts/{framework}')
+def read_posts(framework):
+    return{
+        'posts': [
+            {"title": f"Criando uma aplicação com {framework}", 'date': datetime.now(UTC)},
+            {"title":f"Internacionalizando um aoo {framework}", 'date': datetime.now(UTC)},
+            ]
+        }
